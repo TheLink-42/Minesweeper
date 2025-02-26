4ac9a67d0d25a2cbe87ce141d9aacc37bbf4dcb9
jbaeza-c <jbaeza-c@studen.42madrid.com>
2025-02-26 15:42:52 +0100

    Creation of class UndoList

---
+++ inc/UndoList.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "PosList.h"
+
+int const MAX_UNDO = 100;
+
+class UndoList
+{
+	private:
+		int		count;
+		PosList	list[MAX_UNDO];
+	public:
+		UndoList();
+		~UndoList();
+
+		PosList	get_last() const;
+
+		void	destroy();
+		void	add_last(PosList list);
+};
