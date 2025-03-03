10e950bf21bdf79b870fbd23d9ddfb12b50c53bb
jbaeza-c <jbaeza-c@studen.42madrid.com>
2025-03-03 13:19:56 +0100

    Elaborated a main function with the main loop in which minesweeper will run

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "UndoList.h"
 #include "inputOutput.h"
 
 /*
@@ -14,15 +15,60 @@ int main( void )
 }
 	*/
 
+void	play(Game& game, int x, int y, UndoList& list);
+
 int main( void )
 {
-	Game	game;
+	Game		game;
+	UndoList	undo;
+	int			x,y;
 	
-	display_header();
-	load_game(game);
-	display_game(game);
+	display_intro();
+	if (load_game(game))
+	{
+		display_game(game);
 
-	cout << endl << endl;
-	display_result(game);
+		while (!game.is_complete() && !game.exposed_mine())
+		{
+			ask_pos(x, y);
+			play(game, x, y, undo);
+		}
+		display_result(game);
+	}
+
 	return 0;
+}
+
+void	undo(Game& game, UndoList& undoList)
+{
+	PosList	list = undoList.get_last();
+	int		tiles = list.get_length();
+	for (int i = 0; i < tiles; i++)
+		game.hide(list.get_posx(i), list.get_posy(i));
+	undoList.delete_last();
+}
+
+void	play(Game& game, int x, int y, UndoList& undoList)
+{
+	PosList	list;
+	int		output;
+
+	if (x == -1 && y == -1)
+		game.set_forfeit();
+	else
+	{
+		if (x == -2 && y == -2)
+			game.switch_mode();
+		else if (x == -3 && y == -3)
+			undo(game, undoList);
+		else
+		{
+			output = game.play(x, y, list);
+			if (!output)
+				undoList.add_last(list);
+			else if (output == -1)
+				cout << RED << "Please, restrain coordinates to valid ones" << RESET << endl;
+		}
+		display_game(game);
+	}
 }
